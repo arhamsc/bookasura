d77fa489725f9f39a6a70e0f7c0fb26f03a0fcbd
arhamsc <shaik.arham@gmail.com>
2022-02-19 18:38:46 +0530

    fixes11

--- helpers/from-end/products_prop_funcs.js
+++ helpers/from-end/products_prop_funcs.js
@@ -4,17 +4,18 @@ import { requestUrl } from '../../db/domain_url';
 
 export const fetchAllBooks = async () => {
   const response = await axios.get(requestUrl('api/products'));
-  const books = response.data.products.map((book) =>
-    new Book(
-      book._id,
-      book.name,
-      book.price,
-      book.description,
-      book.category.name,
-      book.imageUrl,
-      book.inventory.quantity,
-    ).getBook(),
-  );
+  const books =
+    response.data.products.map((book) =>
+      new Book(
+        book._id,
+        book.name,
+        book.price,
+        book.description,
+        book.category.name,
+        book.imageUrl,
+        book.inventory.quantity,
+      ).getBook(),
+    ) ?? [];
   const totalBooks = response.data.totalQuantity;
   return { books, totalBooks };
 };
