afdb50318ae6cfcbc2124538f8f5ba9caf7912a8
arhamsc <shaik.arham@gmail.com>
2022-02-19 17:14:55 +0530

    changed url1

--- db/domain_url.js
+++ db/domain_url.js
@@ -1,4 +1,4 @@
 export const requestUrl = (endpoint) => {
-  const url = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  const url = process.env.NEXT_PUBLIC_FRONTEND_URL;
   return `${url}/${endpoint ?? ''}`;
 };
