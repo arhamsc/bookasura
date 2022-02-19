9b99708b649d070a1cd6e43c4678feef4daf0114
arhamsc <shaik.arham@gmail.com>
2022-02-19 17:59:44 +0530

    fixes3

--- db/domain_url.js
+++ db/domain_url.js
@@ -1,4 +1,4 @@
 export const requestUrl = (endpoint) => {
-  const url = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000';
+  const url = process.env.VERCEL_URL || 'http://localhost:3000';
   return `${url}/${endpoint ?? ''}`;
 };
